3defc95c5f4a8e29a053bfbc8881e18381a55ff1
Midety <4erVlad@gmail.com>
2022-03-30 00:39:21 +0300

    CastomHook

--- src/scenes/MainPage/components/Apps/AppsContainer.js
+++ src/scenes/MainPage/components/Apps/AppsContainer.js
@@ -1,13 +1,5 @@
-import { useEffect, useState } from "react";
 import { Apps } from "./Apps";
-import { getApps } from "api/app";
 
-export const AppsContainer = () => {
-  const [apps, setApps] = useState([]);
-
-  useEffect(() => {
-    getApps().then((value) => setApps(value));
-  }, []);
-
-  return <Apps list={apps} />;
+export const AppsContainer = ({ id, name, applications }) => {
+  return <Apps list={applications} />;
 };
